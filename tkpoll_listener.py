bd235b6a27ba10007eaf3d9745e9ae903aac0e39
Technic_bot <technic.robot.35@gmail.com>
2025-02-09 23:12:53 -0600

    Poll command working

--- tkpoll_listener.py
+++ tkpoll_listener.py
@@ -13,18 +13,41 @@ def proc_opts():
     parser = argparse.ArgumentParser()
     parser.add_argument('--token')
     parser.add_argument('--logfile')
+    parser.add_argument('--poll-dir')
+    parser.add_argument('--suggestions')
     return parser.parse_args()
 
 
 class tkPollsLister():
-    def __init__(self, spectate=False, sess_token=None, logfile=''):
+    def __init__(self, 
+            spectate=False, sess_token=None, logfile='',
+            poll_dir='', sugg_file=None):
         self.uri = "wss://tkpolls.com/voter"
-        if spectate:
+        if not sess_token:
             uri = "wss://tkpolls.com/spectatorws"
         self.token = sess_token
         self.logfile = logfile
+        self.poll_dir = poll_dir
         self.make_head();
         self.setup_logging();
+        self.options = {}
+        self.suggs = []
+        self.parse_suggestions(sugg_file)
+        return
+
+    def parse_suggestions(self, file):
+        """ Read all suggestion in file turn to lower case and remove
+            stray white space"""
+        if not file:
+            return 
+
+        with open(file, 'r') as sug_file:
+            self.logger.info(f"Reading suggestion file: {file}")
+            for line in sug_file:
+                sugg = line.lower()
+                sugg = sugg.strip()
+                self.suggs.append(sugg)
+                self.logger.info(f"Got user suggestion: {sugg}")
         return
 
     def setup_logging(self):
@@ -77,6 +100,41 @@ class tkPollsLister():
             "Upgrade": "websocket"}
         return
 
+    async def vote(self):
+        """ From page source return a string of characters corresponding to votes"""
+        if not self.suggs:
+            return
+
+        logger.info(f"Final vote string: {self.vote_str}")
+        await websocket.send(f'ballot: {self.vote_str}')
+        return 
+
+    def store_polls(self, response):
+        if self.poll_dir:
+            poll_end = datetime.fromtimestamp(response['Ends'])
+            poll_end_str = poll_end.strftime("%Y_%m_%d__%H_%M_%S")
+            title = response['Title'].replace(' ', '_')
+            filename = self.poll_dir + title + "_" + poll_end_str + ".json"
+            self.logger.info(f"Saving to {filename}")
+            with open(filename,'w') as output_file:
+                 json.dump(response, output_file)
+        return
+
+    def get_votes(self):
+        vote_str = ''
+        for sug in self.suggs:
+            for letter, opt in self.options.items():
+                proc_opt = opt.lower().strip()
+                if sug in proc_opt:
+                    self.logger.info(
+                        f"Found matching suggestion @ {letter}: {sug} -> {opt}")
+                    letter_int = int(letter) 
+                    letter_char = chr(letter_int) 
+                    vote_str += letter_char
+        self.vote_str = vote_str
+        self.logger.info(f"Total suggestions string: {self.vote_str}")
+        return
+    
     async def connect(self):
         self.logger.info(f"Connecting to {self.uri}")
         async with websockets.connect(
@@ -95,8 +153,13 @@ class tkPollsLister():
                     mtype = response['Mtype']
                     self.logger.info(f"Got message type: {mtype}")
                     self.logger.info(f"{response}")
-                    # with open(filename,'w') as output_file:
-                    #     json.dump(response, output_file)
+                    if mtype == 'poll':
+                        self.options = response['Options']
+                        self.store_polls(response)
+                        self.get_votes()
+                    if mtype == 'call':
+                        self.logger.info("Sending votes")
+                        await self.vote()
                 except ValueError as e:
                     self.logger.info(f"Heartbeat: {message}")
                 except KeyError as e:
@@ -105,5 +168,8 @@ class tkPollsLister():
 
 if __name__ == "__main__":
     args = proc_opts()
-    listener = tkPollsLister(sess_token=args.token) 
+    listener = tkPollsLister(
+            sess_token=args.token,
+            poll_dir='./polls/',
+            sugg_file=args.suggestions) 
     asyncio.run(listener.connect())
